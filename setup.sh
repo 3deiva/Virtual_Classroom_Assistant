902acf4cfe77c59d7f18cea7e297704c8e1fa324
Deiva Raja B <151660497+3deiva@users.noreply.github.com>
2025-01-04 01:42:55 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 # Update pip to the latest version
 pip install --upgrade pip
 
+# Install build-essential to ensure gcc and other tools are present
+apt-get update && apt-get install -y build-essential
+
 # Install numpy with the --prefer-binary flag to avoid building from source
 pip install numpy --prefer-binary
 
