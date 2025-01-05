1a41dd83aaf84e856820c8b0f17788c8a7b5e0d9
Deiva Raja B <151660497+3deiva@users.noreply.github.com>
2025-01-06 01:57:24 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Update pip to the latest version
+
 pip install --upgrade pip
 
-# Install required system dependencies
+
 apt-get update && apt-get install -y build-essential python3-dev
 
-# Install numpy first to avoid conflicts
-pip install numpy --prefer-binary
-
-# Install dependencies from requirements.txt
 pip install -r requirements.txt
