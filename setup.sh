7f26f3718095f8580e54a51f82dc9c7a1bccfcb3
Deiva Raja B <151660497+3deiva@users.noreply.github.com>
2025-01-06 23:18:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-
-pip install --upgrade pip
-
+pip install --upgrade pip setuptools wheel
 
 apt-get update && apt-get install -y build-essential python3-dev libatlas-base-dev gfortran
 
-
-pip install numpy==1.24.3  
-
+pip install numpy==1.24.3
 
 pip install -r requirements.txt
 
- pip install opencv-python-headless==4.5.3.56 --no-binary opencv-python-headless
+pip install opencv-python-headless==4.5.3.56
 
 echo "Setup completed successfully!"
